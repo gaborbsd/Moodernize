c35b8322db28b862d4f3931af867855d1d085c11
Norbert Somogyi <norbertss11@gmail.com>
2019-03-07 15:56:54 +0100

    Removed the method class RewriteOogen

--- hu.bme.aut.moodernize.c2j/src/hu/bme/aut/moodernize/c2j/CToJavaTransformer.java
+++ hu.bme.aut.moodernize.c2j/src/hu/bme/aut/moodernize/c2j/CToJavaTransformer.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.Set;
 
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 
 import hu.bme.aut.oogen.OOClass;
+import hu.bme.aut.oogen.OOMember;
 import hu.bme.aut.oogen.OOMethod;
 import hu.bme.aut.oogen.OOModel;
+import hu.bme.aut.oogen.OOPackage;
+import hu.bme.aut.oogen.OOVariable;
+import hu.bme.aut.oogen.OOVisibility;
 import hu.bme.aut.oogen.OogenFactory;
 import util.Callgraph;
 
@@ -33,8 +38,42 @@ public class CToJavaTransformer implements ICToJavaTransformer {
 			structs.add(cl);
 		}
 		
-		RewriteOOGen.rewrite(model, structs);
+		rewrite(model, structs);
 		return model;
+	}
+	
+	private static void rewrite(OOModel model, List<OOClass> structs) {
+		OOPackage pkg = factory.createOOPackage();
+		pkg.setName("prog");
+		
+		model.getPackages().add(pkg);
+		
+		OOClass cl = factory.createOOClass();
+		cl.setName("ModernizedCProgram");
+		cl.setPackage(pkg);
+		
+		pkg.getClasses().add(cl);
+		
+		for (OOMethod f : model.getGlobalFunctions()) {
+			OOMethod m = (OOMethod)EcoreUtil.copy(f);
+			cl.getMethods().add(m);
+		}
+		model.getGlobalFunctions().clear();
+		
+		for (OOVariable v : model.getGlobalVariables()) {
+			OOMember m = factory.createOOMember();
+			m.setName(v.getName());
+			m.setType(v.getType());
+			m.setVisibility(OOVisibility.PRIVATE);
+			cl.getMembers().add(m);
+		}
+		model.getGlobalVariables().clear();
+		
+		for (OOClass s : structs) {
+			OOClass struct = (OOClass)EcoreUtil.copy(s);
+			struct.setPackage(pkg);
+			pkg.getClasses().add(struct);
+		}
 	}
 	
 	private static void removeNonCustomFunctionsFromCG(Callgraph cg, List<OOMethod> globalFunctions) {
