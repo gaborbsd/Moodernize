79327582cd416b30e8f6ae57f726bcc970f6a179
Norbert Somogyi <norbertss11@gmail.com>
2019-05-10 13:06:48 +0200

    Added binary expressions: bitwise AND, bitwise OR, bitwise XOR, shift left, shift right

--- hu.bme.aut.moodernize.c2j/src/hu/bme/aut/moodernize/c2j/converter/expression/BinaryExpressionConverter.java
+++ hu.bme.aut.moodernize.c2j/src/hu/bme/aut/moodernize/c2j/converter/expression/BinaryExpressionConverter.java
@@ -1,18 +1,12 @@
 package hu.bme.aut.moodernize.c2j.converter.expression;
 
-import java.util.Dictionary;
-
 import org.eclipse.cdt.core.dom.ast.IASTBinaryExpression;
 
-import hu.bme.aut.oogen.OOAdditionExpression;
-import hu.bme.aut.oogen.OOAndExpression;
 import hu.bme.aut.oogen.OOAssignmentExpression;
-import hu.bme.aut.oogen.OODivisionExpression;
 import hu.bme.aut.oogen.OOExpression;
 import hu.bme.aut.oogen.OOLogicalExpression;
-import hu.bme.aut.oogen.OOMultiplicationExpression;
-import hu.bme.aut.oogen.OOOrExpression;
-import hu.bme.aut.oogen.OOSubtractionExpression;
+import hu.bme.aut.oogen.OOTwoOperandArithmeticExpression;
+import hu.bme.aut.oogen.OOTwoOperandLogicalExpression;
 import hu.bme.aut.oogen.OogenFactory;
 
 public class BinaryExpressionConverter {
@@ -34,50 +28,63 @@ public class BinaryExpressionConverter {
 			assignmentExpression.setLeftSide(lhs);
 			assignmentExpression.setRightSide(rhs);
 			return assignmentExpression;
-			
+
 		case IASTBinaryExpression.op_plus:
-			OOAdditionExpression additionExpression = factory.createOOAdditionExpression();
-			additionExpression.setLeftSide(lhs);
-			additionExpression.setRightSide(rhs);
-			return additionExpression;
-			
+			return setBothSidesAndReturn(factory.createOOAdditionExpression(), lhs, rhs);
+
 		case IASTBinaryExpression.op_minus:
-			OOSubtractionExpression substractionExpression = factory.createOOSubtractionExpression();
-			substractionExpression.setLeftSide(lhs);
-			substractionExpression.setRightSide(rhs);
-			return substractionExpression;
-			
+			return setBothSidesAndReturn(factory.createOOSubtractionExpression(), lhs, rhs);
+
 		case IASTBinaryExpression.op_multiply:
-			OOMultiplicationExpression multiExpression = factory.createOOMultiplicationExpression();
-			multiExpression.setLeftSide(lhs);
-			multiExpression.setRightSide(rhs);
-			return multiExpression;
-			
+			return setBothSidesAndReturn(factory.createOOMultiplicationExpression(), lhs, rhs);
+
 		case IASTBinaryExpression.op_divide:
-			OODivisionExpression divisionExpression = factory.createOODivisionExpression();
-			divisionExpression.setLeftSide(lhs);
-			divisionExpression.setRightSide(rhs);
-			return divisionExpression;
-						
+			return setBothSidesAndReturn(factory.createOODivisionExpression(), lhs, rhs);
+
 		case IASTBinaryExpression.op_logicalAnd:
-			if (!(lhs instanceof OOLogicalExpression) || !(rhs instanceof OOLogicalExpression)) {
-				throw new IllegalArgumentException("Both sides of an AndExpression must be of type LogicalExpression.");
-			}
-			OOAndExpression andExpression = factory.createOOAndExpression();
-			andExpression.setLeftSide((OOLogicalExpression) lhs);
-			andExpression.setRightSide((OOLogicalExpression) rhs);
-			return andExpression;
-			
+			return setBothSidesAndReturn(factory.createOOAndExpression(), lhs, rhs);
+
 		case IASTBinaryExpression.op_logicalOr:
-			if (!(lhs instanceof OOLogicalExpression) || !(rhs instanceof OOLogicalExpression)) {
-				throw new IllegalArgumentException("Both sides of an OrExpression must be of type LogicalExpression.");
-			}
-			OOOrExpression orExpression = factory.createOOOrExpression();
-			orExpression.setLeftSide((OOLogicalExpression) lhs);
-			orExpression.setRightSide((OOLogicalExpression) rhs);
-			return orExpression;			
+			return setBothSidesAndReturn(factory.createOOOrExpression(), lhs, rhs);
+
+		case IASTBinaryExpression.op_binaryAnd:
+			return setBothSidesAndReturn(factory.createOOBitwiseAndExpression(), lhs, rhs);
+
+		case IASTBinaryExpression.op_binaryOr:
+			return setBothSidesAndReturn(factory.createOOBitwiseOrExpression(), lhs, rhs);
+
+		case IASTBinaryExpression.op_binaryXor:
+			return setBothSidesAndReturn(factory.createOOBitwiseXorExpression(), lhs, rhs);
+
+		case IASTBinaryExpression.op_shiftLeft:
+			return setBothSidesAndReturn(factory.createOOBitWiseLeftShift(), lhs, rhs);
 			
-		default: throw new UnsupportedOperationException("The following binary expression is not yet supported: " + operator);
+		case IASTBinaryExpression.op_shiftRight:
+			return setBothSidesAndReturn(factory.createOOBitWiseRightShift(), lhs, rhs);
+
+		default:
+			throw new UnsupportedOperationException(
+					"The following binary expression is not yet supported: " + operator);
 		}
+	}
+
+	private OOExpression setBothSidesAndReturn(OOTwoOperandArithmeticExpression expression, OOExpression lhs,
+			OOExpression rhs) {
+		expression.setLeftSide(lhs);
+		expression.setRightSide(rhs);
+		
+		return expression;
+	}
+	
+	private OOExpression setBothSidesAndReturn(OOTwoOperandLogicalExpression expression, OOExpression lhs,
+			OOExpression rhs) {
+		if (!(lhs instanceof OOLogicalExpression) || !(rhs instanceof OOLogicalExpression)) {
+			throw new IllegalArgumentException("Both sides of an AndExpression must be of type LogicalExpression.");
+		}
+		
+		expression.setLeftSide((OOLogicalExpression) lhs);
+		expression.setRightSide((OOLogicalExpression) rhs);
+		
+		return expression;
 	}
 }
